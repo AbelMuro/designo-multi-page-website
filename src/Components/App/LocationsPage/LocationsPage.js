433a3ec33ce0d92ffd40e47f5b2b966e699e6faf
abelmuro <abelmuro93@gmail.com>
2023-03-09 13:47:00 -0800

    commit

--- src/Components/App/LocationsPage/LocationsPage.js
+++ src/Components/App/LocationsPage/LocationsPage.js
@@ -10,7 +10,7 @@ function LocationsPage() {
                 addressTitle='Designo Central Office' 
                 address='3886 Wellington Street Toronto, Ontario M9C 3J5'
                 contactInfo='P : +1 253-863-8967M : contact@designo.co'
-                />
+            />
         </main>
     )
 }
